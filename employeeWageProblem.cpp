059039c4e39dd326d12d81b21010b54e82c61d9d
Om GARG <101242375+Om0978@users.noreply.github.com>
2025-02-12 11:24:04 +0530

    modify:modify employeeWageProblem.cpp with use-case-3

--- employeeWageProblem.cpp
+++ employeeWageProblem.cpp
@@ -21,6 +21,11 @@ int calculateDailyWages(int wagePerHour , int workingHour){
     return wagePerHour*workingHour;
 }
 
+int getPartTimeWorkingHour(){
+    int r = rand()%8+1;
+    return r;
+}
+
 int main()
 {
     cout << "Welcome to Employee-Wage-computation" << endl ;
@@ -31,8 +36,12 @@ int main()
     // calculate daily wages
     int wagePerHour = 20;
     int workingHour = 8;
+    int partTimeWorkingHour = getPartTimeWorkingHour();
     int dailyWage = calculateDailyWages(wagePerHour,workingHour);
     cout<< "daily wage of employee is :"<<dailyWage<<endl;
+    int partTimeWorkingWage = calculateDailyWages(wagePerHour,partTimeWorkingHour);
+    cout<<"partTime wage of employee is : "<<partTimeWorkingWage<<endl;
+    cout<<"total wage of employee is : "<<dailyWage+partTimeWorkingWage<<endl;
 
     return 0;
 }
