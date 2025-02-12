016b90fd1a32b8bb53c3aad2920323ed43ab00c0
Om GARG <101242375+Om0978@users.noreply.github.com>
2025-02-12 14:23:38 +0530

    modify:modify employeeWageProblem.cpp with use-case-7

--- employeeWageProblem.cpp
+++ employeeWageProblem.cpp
@@ -4,94 +4,103 @@
 
 using namespace std;
 
-// Function to calculate employee attenedence
-int markAttendance()
+class ComputeEmployee
 {
-    srand(time(0));
-    int random = rand() % 2;
-    return random;
-}
-
-// Function to calculate employee wage
-int calculateDailyWages(int wagePerHour, int workingHour)
-{
-    return wagePerHour * workingHour;
-}
-
-// Function to get random working hours for part-time employee
-int getRandomWorkingHour()
-{
-    int r = rand() % 8 + 1;
-    return r;
-}
-
-int main()
-{
-    cout << "Welcome to Employee-Wage-computation" << endl;
-
-    const int totalDays = 20; // total working-days of month
+private:
+    const int totalDays = 20;       // Total working days of the month
+    const int wagePerHour = 20;     // Wage per hour
+    const int maxWorkingHours = 100;
     int totalFullTimeEmployeeWage = 0;
     int totalPartTimeEmployeeWage = 0;
-    int maxWorkingHours = 100;
     int totalWorkingHours = 0;
 
-
-    for (int i = 1; i <= totalDays; i++)
+    // Function to calculate employee attendance
+    int markAttendance()
     {
+        return rand() % 2;
+    }
 
-        // attendence
-        int attendance = markAttendance();
+    // Function to calculate employee wage
+    int calculateDailyWages(int workingHour)
+    {
+        return wagePerHour * workingHour;
+    }
 
-        const int wagePerHour = 20;
-        int dailyWage = 0;
-        int workingHour = 0;
-        int partTimeWage = 0;
+    // Function to get random working hours for part-time employee
+    int getRandomWorkingHour()
+    {
+        return rand() % 8 + 1;
+    }
 
-        if(totalWorkingHours>=maxWorkingHours){
-            cout<<"max working hours reached";
-            break;
-        }
-        switch (attendance)
+public:
+    ComputeEmployee()
+    {
+        srand(time(0)); // Seed for randomization
+    }
+
+    // function to compute employee wages
+    void computeWage()
+    {
+        for (int i = 1; i <= totalDays; i++)
         {
-        case 0: // employee is absent
-            //cout << "employee is absent " << endl;
-            break;
+            // Attendance
+            int attendance = markAttendance();
+            int dailyWage = 0;
+            int workingHour = 0;
+            int partTimeWage = 0;
 
-        case 1: // employee is present
-
-            // calculate daily wages
-            int fullTimeWorkingHr=8;
-            workingHour = getRandomWorkingHour(); // for partTime employee
-
-            if(totalWorkingHours+fullTimeWorkingHr>maxWorkingHours){ // calculate wage for remaining days 
-                fullTimeWorkingHr=maxWorkingHours-totalWorkingHours;
-                dailyWage = calculateDailyWages(wagePerHour, fullTimeWorkingHr);
+            if (totalWorkingHours >= maxWorkingHours)
+            {
+                cout << "Max working hours reached" << endl;
                 break;
             }
 
-            if(totalWorkingHours<maxWorkingHours-8){
-                dailyWage = calculateDailyWages(wagePerHour, fullTimeWorkingHr); // for fullTime employee
-                totalWorkingHours+=8;
-                // cout << "daily wage of fullTime employee is :" << dailyWage << endl;
-            }
+            switch (attendance)
+            {
+            case 0: // Employee is absent
+                break;
+            case 1: // Employee is present
+                int fullTimeWorkingHr = 8;
+                workingHour = getRandomWorkingHour(); // For part-time employee
 
-            if(totalWorkingHours<maxWorkingHours-workingHour){
-                totalWorkingHours+=workingHour;
-                partTimeWage = calculateDailyWages(wagePerHour, workingHour);
-                // cout << "partTime employee work for " << workingHour << "hr" << endl;
-                // cout << "partTime wage of employee is : " << partTimeWage << endl;
-            }
+                if (totalWorkingHours + fullTimeWorkingHr > maxWorkingHours)
+                {
+                    fullTimeWorkingHr = maxWorkingHours - totalWorkingHours;
+                }
 
-            totalFullTimeEmployeeWage+=dailyWage;
-            totalPartTimeEmployeeWage+=partTimeWage;
-            break;
+                if (totalWorkingHours < maxWorkingHours - 8)
+                {
+                    dailyWage = calculateDailyWages(fullTimeWorkingHr);
+                    totalWorkingHours += fullTimeWorkingHr;
+                }
+
+                if (totalWorkingHours < maxWorkingHours - workingHour)
+                {
+                    totalWorkingHours += workingHour;
+                    partTimeWage = calculateDailyWages(workingHour);
+                }
+
+                totalFullTimeEmployeeWage += dailyWage;
+                totalPartTimeEmployeeWage += partTimeWage;
+                break;
+            }
         }
-
     }
 
-    cout<<"full-time employee monthly wage is :"<<totalFullTimeEmployeeWage<<endl;
-    cout<<"part-time employee monthly wage is :"<<totalPartTimeEmployeeWage<<endl;
-    cout<<"total month waige is : "<<totalFullTimeEmployeeWage+totalPartTimeEmployeeWage<<endl;
+    // function to display final wages
+    void displayWages()
+    {
+        cout << "Full-time employee monthly wage: " << totalFullTimeEmployeeWage << endl;
+        cout << "Part-time employee monthly wage: " << totalPartTimeEmployeeWage << endl;
+        cout << "Total monthly wage: " << totalFullTimeEmployeeWage + totalPartTimeEmployeeWage << endl;
+    }
+};
 
+int main()
+{
+    cout << "Welcome to Employee-Wage Computation" << endl;
+    ComputeEmployee emp;
+    emp.computeWage();
+    emp.displayWages();
     return 0;
 }
